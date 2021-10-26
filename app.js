f47229f5a4103e16c356c3982ece2da2faa44f84
qasimmalik8555 <qasimmailk8404@gmail.com>
2021-10-26 08:51:04 -0700

    work complete

---
+++ app.js
@@ -0,0 +1,182 @@
+// alert("HELLO CLASS...");
+
+
+// var firstName  = "Qasim"; 
+// var age = 18; 
+// var lastName = "Ali";
+
+
+
+// alert(firstName)
+
+
+
+
+
+
+// var num1 = 200;
+// var num2 = 300;
+
+// var add = num1 + num2
+
+
+// alert(add)          
+
+
+
+// var num1 = 200;
+// var num2 = 300;
+
+// var sub = num1 - num2          
+
+// alert(sub)          
+
+
+
+
+
+
+// var mul = 10 % 3 
+
+// alert(mul)          
+
+
+
+
+// var result1 = 2 * 6 - 30 + (10 * 10)
+
+// alert(result1)
+
+
+
+
+
+
+
+
+
+// var num1 = "10"; ///string
+// var num2 = "20";  ///number
+
+// alert(num1 * num2)
+
+
+
+
+
+
+// var a = 10 // 11
+// var result = a++   +  ++ a
+// alert(result)
+
+// var a = 10
+
+// alert(++a)
+
+
+
+// var a = 10;
+
+// a = a + 10;
+
+// alert(a)
+
+
+// var a = 10 ;
+
+// var result = a++ + a++ + a
+
+// alert(result) 
+
+
+// var a = 10;  
+
+// var result = a++   +    ++a    +    a    +     a++
+
+// alert(result)
+
+
+
+
+
+
+// var a  = 30;
+
+// var result = a++ + a++ - a - ++a + a++
+  
+// alert(result)
+
+
+
+
+// var a = 13 ; 
+// var result = a++  +  --a  +  ++a - a++ + a--       
+
+
+
+// alert(result) 
+
+
+
+// var a = 5;
+// var result = a++ - --a + --a + a++ + a + a +  ++a 
+
+
+// alert(result)
+
+
+
+
+
+
+ var userName = prompt("Enter your name: ");
+
+ 
+ document.write(  "name: " +  userName  + "<br>")
+
+
+ var rollnumber = prompt("Enter your rollnumber: ");
+
+ 
+ document.write(  "rollnumber: " + rollnumber  + "<br>")
+
+ var clas = prompt("Enter your class: ");
+
+ document.write( "class: " + clas  + "<br>")
+
+
+ var Maths = prompt("Enter your Maths marks: ");
+
+ 
+ document.write(  "your Maths mark is: " + Maths  + "<br>")
+
+
+ var English = prompt("Enter your English marks: ");
+
+ 
+ document.write( "your English mark  is: " + English  + "<br>")
+
+
+
+ var Urdu = prompt("Enter your URdu marks: ");
+
+ document.write( "your urdu mark is: " + Urdu  + "<br>")
+
+ var Islamait = prompt("Enter your Islamait marks: ");
+
+ document.write( "your islmait mark is: " + Islamait + "<br>")
+
+
+ var Pakstudies = prompt("Enter your Pakstudies marks: ");
+
+ document.write( "your pakstudies mark is: " + Pakstudies  + "<br>")
+
+
+ var obtaine_marks = parseInt(Maths) + parseInt(English) + parseInt(Urdu) +parseInt(Islamait) +parseInt(Pakstudies)
+ 
+ document.write( "your obtain_mark is: " + obtaine_marks  + "<br>")
+
+
+var percentage = obtaine_marks * 100 / 500
+
+document.write( "your percentage is: " + percentage  + "<br>")
